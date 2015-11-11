14b4a8835d0e1db2606e7ed0f47d0ec5f5759815
akameco <akameco.t@gmail.com>
2015-11-12 01:18:51 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 PWD=`pwd`
 
-ln -sf ${PWD}/.gitconfig ~/
-ln -sf ${PWD}/.vimrc ~/
-ln -sf ${PWD}/.gvimrc ~/
-ln -sf ${PWD}/.vimshrc ~/
-ln -sf ${PWD}/.vimperatorrc ~/
-ln -sf ${PWD}/.gvimrc ~/
-ln -sf ${PWD}/.zshrc ~/
+ln -sf ${PWD}/zshrc ~/.zshrc
+ln -sf ${PWD}/zshenv ~/.zshenv
+ln -sf ${PWD}/zprofile ~/.zprofile
+ln -sf ${PWD}/gitconfig ~/.gitconfig
+ln -sf ${PWD}/vimrc ~/.vimrc
+ln -sf ${PWD}/gvimrc ~/.gvimrc
+ln -sf ${PWD}/vimperatorrc ~/.vimperatorrc
 ln -sf ${PWD}/vim/snippets ~/.vim/snippets
 
 # install Neobundle
-[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 echo complete setting!
