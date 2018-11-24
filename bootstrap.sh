91025c877544ba8c9a619aa82b87a94766d19124
akameco <akameco.t@gmail.com>
2018-11-24 18:06:19 +0900

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,20 +11,40 @@ ln -sf ${PWD}/.gitconfig ~/.gitconfig
 ln -sf ${PWD}/.vim/snippets ~/.vim/snippets
 ln -sf ${PWD}/gitmessage ~/.gitmessage
 
+# brewがなければbrewを入れる
+if [ ! `which brew` ]; then
+  /usr/bin/ruby -e `curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install`
+  brew upgrade
+  brew bundle
+fi
+
 # Install Neobundle
 if [ ! -d ~/.vim/bundle ]; then
   mkdir -p ~/.vim/bundle
   git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
   echo 'Install NeoBundle'
-fi;
-
-# npm
-if [ `which npm` ]; then
-  cat ${PWD}/list/npm-global | xargs npm i -g
 fi
 
-if [ `which ghq` ]; then
-  ghq import -p < ${PWD}/list/sh-list
-fi
+__setup() {
+  # update config
+  # git pull origin master;
 
-# git pull origin master;
+  # Brew
+  brew upgrade
+  brew bundle
+
+  # zsh
+  if [ `which ghq` ]; then
+    ghq import -p < ${PWD}/list/sh-list
+  fi
+
+  # npm
+  if [ `which npm` ]; then
+    cat ${PWD}/list/npm-global | xargs npm i -g
+  fi
+}
+
+
+if [ "$1" == "-f" ]; then
+  __setup
+fi
